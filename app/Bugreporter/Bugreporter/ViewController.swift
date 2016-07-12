61e9488ac35120dac52103b485270abb323fd460
Leonard Thomas <leo.thomas@me.com>
2016-07-12 10:55:42 +0200

    init project

---
+++ app/Bugreporter/Bugreporter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bugreporter
+//
+//  Created by Leo Thomas on 12/07/16.
+//  Copyright © 2016 Leonard Thomas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
